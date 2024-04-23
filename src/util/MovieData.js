30b5a927cda11611fca47504a0aef95602fffb7f
Dmitry <free-doom46@mail.ru>
2024-04-23 13:14:46 +0300

    refactor: del favorite in data

--- src/util/MovieData.js
+++ src/util/MovieData.js
@@ -3,57 +3,49 @@ const MOVIE_DATA = [
 		id: 1,
     title: 'Black Widow',
     poster: '/src/assets/posters/Black-Widow.jpg',
-    rating: 324,
-    favorite: false
+    rating: 324
   },
   {
 		id: 2,
     title: 'Shang Chi',
     poster: '/src/assets/posters/Shang-Chi.jpg',
-    rating: 124,
-    favorite: false
+    rating: 124
   },
   {
 		id: 3,
     title: 'Loki',
     poster: '/src/assets/posters/Loki.jpg',
-    rating: 235,
-    favorite: false
+    rating: 235
   },
   {
 		id: 4,
     title: 'How I Met Your Mother',
     poster: '/src/assets/posters/How-I-Met-Your-Mother.jpg',
-    rating: 123,
-    favorite: false
+    rating: 123
   },
   {
 		id: 5,
     title: 'Money Heist',
     poster: '/src/assets/posters/Money-Heist.jpg',
-    rating: 8125,
-    favorite: true
+    rating: 8125
   },
   {
 		id: 6,
     title: 'Friends',
     poster: '/src/assets/posters/Friends.jpg',
-    rating: 123,
-    favorite: false
+    rating: 123
   },
   {
 		id: 7,
     title: 'The Big Bang Theory',
     poster: '/src/assets/posters/The-Big-Bang-Theory.jpg',
-    rating: 12,
-    favorite: false
+    rating: 12
   },
   {
 		id: 8,
     title: 'Two And a Half Men',
     poster: '/src/assets/posters/Two-And-a-Half-Men.jpg',
-    rating: 456,
-    favorite: false
+    rating: 456
   }
 ];
 
